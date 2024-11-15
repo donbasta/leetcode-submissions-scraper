789dad7381f0860c2937d5e77a419951422067f0
donbasta <faddilafarras@gmail.com>
2024-11-16 06:41:07 +0700

    fix: change user-agent and catch error for each page when scraping problems

--- src/scrape-problems.ts
+++ src/scrape-problems.ts
@@ -17,72 +17,80 @@ export async function scrapeProblems(fname: string = 'problems.json') {
     let problems: Problem[] = [];
 
     for (let current_page = 1; current_page <= NUM_PAGE; current_page++) {
-      await page.goto(`https://leetcode.com/problemset/?page=${current_page}`);
-      await page.waitForSelector('[role="rowgroup"]');
-
-      const rowGroups = await page.$$('*[role="rowgroup"]');
-      if (rowGroups.length > 0) {
-        const rowGroup = rowGroups[rowGroups.length - 1];
-        const rowElements = await rowGroup.$$(':scope > *');
-
-        console.log(
-          `finding ${rowElements.length} problems in page ${current_page}`
+      try {
+        await page.goto(
+          `https://leetcode.com/problemset/?page=${current_page}`
         );
+        await page.waitForSelector('[role="rowgroup"]');
 
-        for (const rowElement of rowElements) {
-          const columnElements = await rowElement.$$(':scope > *');
+        const rowGroups = await page.$$('*[role="rowgroup"]');
+        if (rowGroups.length > 0) {
+          const rowGroup = rowGroups[rowGroups.length - 1];
+          const rowElements = await rowGroup.$$(':scope > *');
 
-          let is_solved = null;
-          let href = null;
-          let name = null;
-          let difficulty = null;
-          let is_premium = null;
+          console.log(
+            `finding ${rowElements.length} problems in page ${current_page}`
+          );
 
-          const secondColumnElement = columnElements[1];
-          const linkElement = await secondColumnElement.$('a');
-          if (linkElement) {
-            href = await linkElement.evaluate((node) => node.href);
-            name = await linkElement.evaluate((node) => node.text);
-          }
-          const premiumLogoElement = await secondColumnElement.$('svg');
-          if (premiumLogoElement) {
-            is_premium = true;
-          } else {
-            is_premium = false;
-          }
+          for (const rowElement of rowElements) {
+            const columnElements = await rowElement.$$(':scope > *');
 
-          const firstColumnElement = columnElements[0];
-          const greenCheckMarkElement = await firstColumnElement.$('svg');
+            let is_solved = null;
+            let href = null;
+            let name = null;
+            let difficulty = null;
+            let is_premium = null;
 
-          if (greenCheckMarkElement && is_premium !== null && !is_premium) {
-            is_solved = true;
-          } else {
-            is_solved = false;
-          }
-
-          const fifthColumnElement = columnElements[4];
-          const difficultyElement = await fifthColumnElement.$('span');
-          if (difficultyElement) {
-            difficulty = await difficultyElement.evaluate(
-              (node) => node.textContent
-            );
-            if (difficulty !== null) {
-              difficulty = difficulty as Difficulty;
+            const secondColumnElement = columnElements[1];
+            const linkElement = await secondColumnElement.$('a');
+            if (linkElement) {
+              href = await linkElement.evaluate((node) => node.href);
+              name = await linkElement.evaluate((node) => node.text);
             }
+            const premiumLogoElement = await secondColumnElement.$('svg');
+            if (premiumLogoElement) {
+              is_premium = true;
+            } else {
+              is_premium = false;
+            }
+
+            const firstColumnElement = columnElements[0];
+            const greenCheckMarkElement = await firstColumnElement.$('svg');
+
+            if (greenCheckMarkElement && is_premium !== null && !is_premium) {
+              is_solved = true;
+            } else {
+              is_solved = false;
+            }
+
+            const fifthColumnElement = columnElements[4];
+            const difficultyElement = await fifthColumnElement.$('span');
+            if (difficultyElement) {
+              difficulty = await difficultyElement.evaluate(
+                (node) => node.textContent
+              );
+              if (difficulty !== null) {
+                difficulty = difficulty as Difficulty;
+              }
+            }
+
+            problems.push({
+              name,
+              difficulty,
+              is_solved,
+              source_code: null,
+              href,
+              is_premium,
+              is_submission_scraped: false,
+            });
           }
 
-          problems.push({
-            name,
-            difficulty,
-            is_solved,
-            source_code: null,
-            href,
-            is_premium,
-            is_submission_scraped: false,
-          });
+          fs.writeFileSync(fname, JSON.stringify(problems, null, 2));
         }
-
-        fs.writeFileSync(fname, JSON.stringify(problems, null, 2));
+      } catch (err) {
+        console.error(
+          `[scrapeProblems(${fname}$)]: Page ${current_page}: ${err}`
+        );
       }
     }
     console.log('successfully scrape all problems');
--- src/utils.ts
+++ src/utils.ts
@@ -7,7 +7,8 @@ export async function init() {
   const page = await browser.newPage();
   await page.setUserAgent(
     // 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
+    // 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
   );
 
   return { browser, page };
